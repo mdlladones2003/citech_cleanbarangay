ee20cfbf04ee0d8f0a8cd06d4312a4014d489fb5
Mark Anthony <lladones021@gmail.com>
2026-09-22 12:46:25 +0800

    cleaned index

--- index.php
+++ index.php
@@ -5,14 +5,6 @@
         <h1>Dashboard</h1>
         <p>Welcome to the system.</p>
         <button class="btn btn-primary">Get Started</button>
-        <button class="btn btn-neutral">Neutral</button>
-<button class="btn btn-primary">Primary</button>
-<button class="btn btn-secondary">Secondary</button>
-<button class="btn btn-accent">Accent</button>
-<button class="btn btn-info">Info</button>
-<button class="btn btn-success">Success</button>
-<button class="btn btn-warning">Warning</button>
-<button class="btn btn-error">Error</button>
     </div>
 </div>
 
